e1dcdb11c55b0074ae51cb1f5cf6edd0a8d869cf
Batsilos <batsilos@gmail.com>
2015-07-22 12:03:22 +0300

    Create vendor Jiayu G4s

---
+++ README.md
@@ -0,0 +1,13 @@
+	# CM 12.1
+
+* Compilation
+
+        # repo init -u git://github.com/CyanogenMod/android.git -b cm-12.1
+        
+        # repo sync
+        
+        # source build/envsetup.sh
+        
+        # brunch cm_g4s-eng
+
+
---
+++ g4s-vendor.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/g4s/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/g4s/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/g4s/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/g4s/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/g4s/framework,system/framework)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/g4s/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/g4s/vendor,system/vendor)
